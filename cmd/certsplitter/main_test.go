b121e3c6c84837d60001c69c90a393563133e130
Josh Russett <jrussett@vmware.com>
2021-05-18 20:10:45 +0000

    Convert pathing for go mod'ed tests
    [#177498057](https://www.pivotaltracker.com/story/show/177498057)

--- cmd/certsplitter/main_test.go
+++ cmd/certsplitter/main_test.go
@@ -24,7 +24,7 @@ var _ = Describe("Certsplitter", func() {
 	BeforeEach(func() {
 		var err error
 		trustedCertsDir = filepath.Join(
-			os.Getenv("GOPATH"),
+			os.Getenv("DIEGO_RELEASE_DIR"),
 			"src",
 			"code.cloudfoundry.org",
 			"certsplitter",
